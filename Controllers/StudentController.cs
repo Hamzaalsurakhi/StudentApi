968b3f32562fa8f20b096e69f041c0d5c2a30ee2
Hamzaalsurakhi <alsurakhihamza@gmail.com>
2024-09-19 10:57:02 +0300

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentApi.DataSimulation;
+using StudentApi.Models;
+
+namespace StudentApi.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/StudentApi")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> GetAllStudents()
+        {
+            return Ok(StudentSimulationData.StudentList);
+        }
+    }
+}
---
+++ DataSimulation/StudentSimulationData.cs
@@ -0,0 +1,20 @@
+﻿using StudentApi.Models;
+
+namespace StudentApi.DataSimulation
+{
+    public class StudentSimulationData
+    {
+        //static list of student
+        public static readonly List<Student> StudentList = new List<Student>
+        {
+            //initiakiza the list with  ssmr student object
+            new Student {Id=1,Name="Hamza",Age=25,Grade=88},
+             new Student {Id=2,Name="ZAID",Age=23,Grade=70},
+              new Student {Id=3,Name="LAITH",Age=21,Grade=90},
+               new Student {Id=4,Name="Abood",Age=20,Grade=77}
+
+
+
+        };
+    }
+}
